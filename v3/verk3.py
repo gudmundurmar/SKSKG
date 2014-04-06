724bae47affb591ea53c5814ec921879cc7a03e9
gudmundurmar <gmg13@hi.is>
2014-04-06 18:52:06 +0000

    Sett í klasa
    Main fall og contstructor búinn til

--- v3/verk3.py
+++ v3/verk3.py
@@ -1,145 +1,162 @@
 # -*- coding: cp1252 -*-
 from priority_dict import priority_dict
+from bisect import bisect_left
+import time
+import re
 
-notSpan = []
-Span = []
+class verk3:
+    def __init__(self):
+        self.notSpan = []
+        self.Span = []
+        self.wholeNet = []
+        self.tree = []
 
-
-def inputToDict(filename):
-    global notSpan
-    global Span
-    dict = {}
-    count = {}
-    file = open(filename).readlines()
-    for line in file:
-        split = line.split(" ")
-        if len(split) > 1:
-            if '\n' in split[2]:
-                split[2] = split[2][:-1]
-                
-            dict[split[0]].append([split[1],split[2]])
-            dict[split[1]].append([split[0],split[2]])
-            notSpan.append([split[0],split[1],split[2]])
-        else:
-            for i in range(0, int(split[0][:-1])):
-                dict[str(i)] = [float("inf"), None]
-
-    weight = MSTPRIM(dict,0, dict['0']);
-    print weight
-    NotMinPRIM(weight)
-
-
-def MSTPRIM(G,w,r):
-    global notSpan
-    global Span
-
-    
-    r[0] = 0
-    Q =  priority_dict(G);
-    while Q:
-        Q._rebuild_heap()
+    def inputToDict(self,filename):
+        dict = {}
         
-        u = Q.smallest()
-
-        if(not(int(u) == 0)):
-            if(int(u) < int(Q[u][1])):
-                Span.append([Q[u][1],u,int(Q[u][0])])
-                notSpan.remove([str(u),str(Q[u][1]),str(int(Q[u][0]))])
+        file = open(filename, "r").readlines()
+        for line in file:
+            split = line.split(" ")
+            if len(split) > 1:
+                if '\n' in split[2]:
+                    split[2] = split[2][:-1]
+                    
+                dict[split[0]].append([split[1], split[2]])
+                dict[split[1]].append([split[0], split[2]])
+                               
+                self.wholeNet.append([int(split[2]),split[0],split[1]])
             else:
-                Span.append([Q[u][1],u,int(Q[u][0])])
-                notSpan.remove([str(Q[u][1]),str(u),str(int(Q[u][0]))])
+                for i in range(0, int(split[0][:-1])):
+                    dict[str(i)] = [float("inf"), None, []]
+                print dict
+
         
-        w += int(Q[u][0])
-        u = Q.pop_smallest()
-        
-        for v in G[u]:
-            if isinstance(v, list):
-                stri = str(v[0])
-                if v[0] in Q:
-                    if float(v[1]) < float(Q[v[0]][0]):
-                        Q[v[0]][1] = u
-                        Q[v[0]][0] = float(v[1])
-
-    return w    
-
-
-def NotMinPRIM(w):
-    global Span
-    global notSpan
-
-    count = 0
-
-    check = 0
-    minEdge = [-1,-1,float("inf")]
-    checkedVertices = []
-    output = []
-    invalidMin = True
-
-    notSmallestSpans = {}
-
-    for e in Span:
-
-        newWeight = w-e[2]
-        check = 1
-
-        if e[0] < e[1]:
-            output = [int(e[0]),int(e[1])]
-        else:
-            output = [int(e[1]),int(e[0])]
-        
-        #output = str(e[0])+" "+str(e[1])+" "
-        for v in Span:
-            if str(e[1]) == str(v[0]):
-                check = 0
-
-        if(check == 1):
-            checkedVertices.append(str(e[0]))
-            checkedVertices.append(str(e[1]))
-        else:
-            checkedVertices.append(str(e[0]))
-
-        for notUsed in notSpan:
-            if e[check] == notUsed[0] or e[check] == notUsed[1]:
-                if float(notUsed[2]) < float(minEdge[2]):
-                    invalidMin = False
-                    minEdge = notUsed
-
-        newWeight += int(minEdge[2])
-        output.append(str(newWeight))
-
-        notSmallestSpans[count] = output
-
-        count += 1
-        
-        toRemove = []
-
-
-        for nUsed in notSpan:
-            if (nUsed[0] in checkedVertices) and (nUsed[1] in checkedVertices):
-                toRemove.append(nUsed)
-                if nUsed == minEdge:
-                    invalidMin = True
-                    minEdge = [-1,-1,float("inf")]
-                    
-        for remove in toRemove:
-            notSpan.remove(remove)
-                    
-        if invalidMin:
-            for findMin in notSpan:
-                    if float(findMin[2]) < float(minEdge[2]):
-                        invalidMin = False            
-                        minEdge = findMin
-
-
-        #Span.remove(e)
+        self.wholeNet.sort(reverse=True)
+        weight = self.MSTPRIM(dict,0, dict['0']);
+        print weight
+        #print dict
+        self.NotMinPRIM(dict, weight)
 
     
-        
-    Q =  priority_dict(notSmallestSpans);
-    while Q:
-        vertex = Q.smallest()
-        print str(Q[vertex][0])+" "+str(Q[vertex][1])+" "+Q[vertex][2]
-        vertex = Q.pop_smallest()
-                
 
-inputToDict("10.in")
+
+    def MSTPRIM(self,G,w,r):
+   
+        r[0] = 0
+        
+        Q =  priority_dict(G);
+        while Q:
+            Q._rebuild_heap()
+            
+            u = Q.pop_smallest()
+
+            if not(G[u][1] is None):
+                if(int(u) < int(G[u][1])):
+                    self.Span.append([int(G[u][0]), u,G[u][1]])
+                    #notSpan[:] = (name for name in notSpan if name != [int(G[u][0]), u,G[u][1]])
+                    #notSpan.remove([int(G[u][0]), u,G[u][1]])
+                else:
+                    self.Span.append([int(G[u][0]), G[u][1], u])
+                    #notSpan[:] = (name for name in notSpan if name != [int(G[u][0]), G[u][1], u])
+                    #notSpan.remove([int(G[u][0]), G[u][1], u])
+            w += int(G[u][0])
+
+            self.tree.append(u)
+            
+            for v in range(3,len(G[u])):
+                ver = G[u][v]
+                if ver[0] in Q:
+                    if float(ver[1]) < float(Q[ver[0]][0]):
+                        Q[ver[0]][1] = u
+                        G[ver[0]][1] = u
+
+                        if float(G[u][0]) == float(0):
+                            G[u][0] = float(ver[1])
+
+                        #setur nýja besta
+                        Q[ver[0]][0] = float(ver[1])
+                        G[ver[0]][0] = float(ver[1])
+        
+
+        for i in range(1,len(self.tree)):
+            G[str(G[self.tree[i]][1])][2].append(self.tree[i])
+
+        return w    
+
+
+    def NotMinPRIM(self,G,w):
+
+        self.Span.sort()
+        
+        for e in self.wholeNet:
+            if binary_search(self.Span, e) == -1:
+                self.notSpan.append(e)
+        
+        cnt = 0
+        nrSecond = 1
+        weight = 0
+        shortest = {}
+
+
+        for i in range(1,len(self.tree)):
+            
+            node = self.tree[i]
+
+            context = doubleBFS(G, node, G[node][1])
+            
+            for j in range(0,len(self.notSpan)):
+                cur = len(self.notSpan)-1-j
+                if self.notSpan[cur][1] in context and self.notSpan[cur][2] in context:
+                    continue
+                elif self.notSpan[cur][1] in context or self.notSpan[cur][2] in context:
+                    weight = w-G[node][0]+self.notSpan[cur][0]
+                    break
+            
+            if(int(node) < int(G[node][1])):
+                shortest[cnt] = [int(node), int(G[node][1]), int(weight)]
+            else:
+                shortest[cnt] = [int(G[node][1]), int(node), int(weight)]
+            cnt += 1
+            
+
+        Q =  priority_dict(shortest);
+
+        while Q:
+            u = Q.smallest()
+            print str(Q[u][0])+" "+str(Q[u][1])+" "+str(Q[u][2])
+            u = Q.pop_smallest()
+        
+
+def doubleBFS(G,u,v):
+    contextU = [u]
+    contextV = [v]
+
+    treeU = [u]
+    treeV = [v]
+    
+    while treeU and treeV:
+        curU = treeU.pop()
+        for adjU in G[curU][2]:
+            treeU.append(adjU)
+            contextU.append(adjU)
+        curV = treeV.pop()
+        for adjV in G[curV][2]:
+            treeV.append(adjV)
+            contextV.append(adjV)
+
+    if len(contextU) < len(contextV):
+        return contextU
+    else:
+        return contextV
+        
+
+def binary_search(a, x, lo=0, hi=None):   # can't use a to specify default for hi
+    hi = hi if hi is not None else len(a) # hi defaults to len(a)   
+    pos = bisect_left(a,x,lo,hi)          # find insertion position
+    return (pos if pos != hi and a[pos] == x else -1) # don't walk off the end    
+    
+if __name__ == '__main__':
+    start_time = time.time()
+    V3 = verk3()
+    V3.inputToDict("simple.in")
+    print time.time() - start_time, "seconds"
